fd694141a01fda05384cbd83af9957774818cfb8
lyricswithsong1308-spec <lyricswithsong1308@gmail.com>
2025-10-01 11:06:49 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'yujanii-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+  '/manifest.json',
+  // add other static files here like CSS/JS/images
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
